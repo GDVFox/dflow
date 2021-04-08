3693c0e58e09746989998d79daa60f16fef51d0f
GDVFox <gaaveer@gmail.com>
2021-04-08 22:22:57 +0300

    [+] machine_node initial

---
+++ machine_node/config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"github.com/GDVFox/dflow/util"
+	"github.com/GDVFox/dflow/util/httplib"
+	"github.com/GDVFox/dflow/util/storage"
+)
+
+// Conf глобальный конфиг синглтон.
+var Conf = NewConfig()
+
+// Config конфигурация сервиса.
+type Config struct {
+	HTTP    *httplib.HTTPConfig `yaml:"http"`
+	Logging *util.LoggingConfig `yaml:"logging"`
+	ETCD    *storage.ETCDConfig `yaml:"etcd"`
+}
+
+// NewConfig создает конфиг с настройками по-умолчанию
+func NewConfig() *Config {
+	return &Config{
+		HTTP:    httplib.NewtHTTPConfig(),
+		Logging: util.NewLoggingConfig(),
+		ETCD:    storage.NewETCDConfig(),
+	}
+}
